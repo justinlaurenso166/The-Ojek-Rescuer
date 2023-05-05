ef36ea74ca098ed97d1ac8bea499500ada6d86d5
Matthew George Wellson <102170142+Kaisarming@users.noreply.github.com>
2023-05-05 20:23:47 +0700

    Splash Screen Icei and Team(Sementara) Added!
    Splash screen team kemungkinan akan ganti. Soalnya logo team cuma dibuat pakai cool text wkwkwk. Nunggu yang design logo kasih logonya.

---
+++ Assets/Script/SplashScreen.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class SplashScreen : MonoBehaviour
+{
+    public Image splashImage1;
+    public Image splashImage2;
+
+    IEnumerator Start()
+    {
+        splashImage1.canvasRenderer.SetAlpha(0.0f);
+        splashImage2.canvasRenderer.SetAlpha(0.0f);
+
+        FadeIn(splashImage1);
+        yield return new WaitForSeconds(2.5f);
+        FadeOut(splashImage1);
+        yield return new WaitForSeconds(2f);
+        FadeIn(splashImage2);
+        yield return new WaitForSeconds(2.5f);
+        FadeOut(splashImage2);
+        yield return new WaitForSeconds(2f);
+        SceneManager.LoadScene("GameMenu");
+    }
+
+    void FadeIn(Image image)
+    {
+        image.CrossFadeAlpha(1.0f, 1.5f, false);
+    }
+    void FadeOut(Image image)
+    {
+        image.CrossFadeAlpha(0.0f, 2.5f, false);
+    }
+}
