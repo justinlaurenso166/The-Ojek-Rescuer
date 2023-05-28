7e7bb251c51f892b841a46acc6ff3d6e05b5e035
Justin Laurenso <justin.laurenso166@gmail.com>
2023-05-28 21:56:36 +0700

    update particle collision
    debug log hanya testing

--- Assets/Scripts/APAR/CollisionDetect.cs
+++ Assets/Scripts/APAR/CollisionDetect.cs
@@ -4,20 +4,6 @@ using UnityEngine;
 
 public class CollisionDetect : MonoBehaviour
 {
-    public ParticleSystem particleSystem;
-
-    // Start is called before the first frame update
-    // void Start()
-    // {
-    //     particleSystem = GetComponent<ParticleSystem>();
-    //     var collisionModule = particleSystem.collision;
-    //     collisionModule.enabled = true;
-    //     collisionModule.type = ParticleSystemCollisionType.World;
-    //     collisionModule.SetPlane(0, GameObject.FindWithTag("Ground").transform);
-
-    //     particleSystem.collision.SetCollisionEventCallback(OnParticleCollision);
-    // }
-
     void OnParticleCollision(GameObject other)
     {
         Debug.Log(other.gameObject.name);
